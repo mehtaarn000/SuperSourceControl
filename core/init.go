6f4dcee9986e03c2bc5c2fc479889037e41a7429
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2021-03-23 15:20:31 -0500

    Add repo existance check

--- core/init.go
+++ core/init.go
@@ -3,6 +3,12 @@ package core
 import "os"
 
 func Init() {
+	if _, err := os.Stat("./conf/app.ini"); err != nil {
+		if os.IsExist(err) {
+			print("Repository already exists.")
+			os.Exit(0)
+		}
+	}
 	os.MkdirAll(".ssc/branches", 0777)
 	os.MkdirAll(".ssc/objects", 0777)
 	os.MkdirAll(".ssc/tmp", 0777)
