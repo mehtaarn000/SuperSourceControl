7449d3c37411b2802fe3aec101203f854e6e01e8
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2021-03-23 00:38:46 -0500

    Add hash-object functions

--- core/hash-object.go
+++ core/hash-object.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"encoding/hex"
+	"io/ioutil"
+	"ssc/zlibutils"
 	"golang.org/x/crypto/ripemd160"
 )
 
@@ -14,8 +16,45 @@ func hashObject(s string) string {
 	return hash
 }
 
-// PrintHash takes a string, turns it into a blob object, and hashes it
-func PrintHash(s string) {
+// PrintHash takes a string from stdin, turns it into a blob object, hashes it, and prints the generated hash
+func PrintStdinHash(s string) {
 	value := hashObject(s)
 	println(value)
+}
+
+// WriteStdinHash takes a string from stdin, turns it into a blob object, hashes it, and writes the object to the ssc database
+func WriteStdinHash(s string, quiet bool) {
+	value := hashObject(s)
+	ioutil.WriteFile(".ssc/objects/" + value, []byte(s), 0644)
+	zlibutils.CompressFile(map[string]string{".ssc/objects/" + value: value})
+
+	if !quiet {
+		println(value)
+	}
+}
+
+// PrintFileHash takes a filename, turns the file into a blob object, hashes it, and prints the generated hash
+func PrintFileHash(filename string) {
+	bytesfilecontent, err := ioutil.ReadFile(filename)
+	filecontent := string(bytesfilecontent)
+
+	println(hashObject(filecontent))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func WriteFileHash(filename string, quiet bool) {
+	bytesfilecontent, err := ioutil.ReadFile(filename)
+	filecontent := string(bytesfilecontent)
+
+	hash := hashObject(filecontent)
+	ioutil.WriteFile(".ssc/objects/" + hash, bytesfilecontent, 0644)
+
+	zlibutils.CompressFile(map[string]string{".ssc/objects/" + hash: hash})
+
+	if err != nil {
+		panic(err)
+	}
 }
