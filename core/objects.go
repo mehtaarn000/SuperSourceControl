4050875ae64d2b785b91cd85983c011f53186c75
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2021-03-30 02:04:44 -0500

    Add descriptive comments to core/objects.go

--- core/objects.go
+++ core/objects.go
@@ -15,6 +15,7 @@ import (
 	"strings"
 )
 
+// Utility function to get all file names in cwd
 func get_files() []string {
 	var files []string
 
@@ -38,6 +39,7 @@ func get_files() []string {
 	return files
 }
 
+// Utility to check if file exists in cwd
 func fileExists(filename string) bool {
 	info, err := os.Stat(filename)
 	if os.IsNotExist(err) {
@@ -48,58 +50,84 @@ func fileExists(filename string) bool {
 
 // CreateCommit creates a commit, compresses it, and writes it to a file.
 func CreateCommit(c Commit) {
+	/* Commit object looks like this
+	tree $treehash
+	date $date
+	branch $branch
+	author $author
+
+	$COMMITMSG
+	*/
+
 	commit := ""
 	commit += "tree " + c.Tree + "\n"
 	commit += "date " + c.Date + "\n"
 	commit += "branch " + c.Branch + "\n\n"
 	commit += c.Message
 
+	/* Commit hash is calculated like:
+	commit $LENGTHOFCOMMITOBJECT $COMMITOBJECT
+	*/
+
 	commitlen := string(rune(len(commit)))
 	lenoflen := len(commitlen)
 	commit = "commit " + commitlen + commit
 
+	// Calculate hash
 	hasher := ripemd160.New()
 	hasher.Write([]byte(commit))
 	hash := hex.EncodeToString(hasher.Sum(nil))
+
+	// Write it to a file
 	filename := ".ssc/objects/" + hash
 	writer, err := os.Create(filename)
-
-	if err != nil {
-		panic(err)
-	}
-
 	writer.WriteString(commit[7+lenoflen:])
 	zlibutils.CompressFile(map[string]string{filename: hash})
-
+	
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
 	defer f.Close()
 
 	f.WriteString(hash + "\n")
 
+	// Write commit to commitlog
 	writeToLog, err := os.OpenFile(".ssc/commitlog", os.O_APPEND|os.O_WRONLY, 0644)
 	defer writeToLog.Close()
-
 	writeToLog.WriteString(hash + "\n")
 
 	println(hash)
+
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 // CreateTree creates a tree object
 func CreateTree() string {
+	// Get all file names in directory
 	files := get_files()
 
+	// Create blobs using all file names
 	rawtree := createBlobs(files)
 	treelength := string(rune(len(rawtree)))
+
+	/*Tree hash is calculated like:
+	tree $LENGTHOFTREE $TREE
+	*/
+
 	tree := "tree " + treelength + rawtree
 
+	// Calculate tree hash
 	hasher := ripemd160.New()
 	hasher.Write([]byte(tree))
 	hash := hex.EncodeToString(hasher.Sum(nil))
 	hashfile := ".ssc/objects/" + hash
 
+	// Write tree to file
 	ioutil.WriteFile(hashfile, []byte(rawtree), 0644)
 	zlibutils.CompressFile(map[string]string{hashfile: hash})
 
+	// Write tree hash to list of trees
 	f, err := os.OpenFile(".ssc/trees", os.O_APPEND|os.O_WRONLY, 0644)
 	defer f.Close()
 
@@ -121,12 +149,18 @@ func createBlobs(filenames []string) string {
 	for index, file := range filenames {
 		raw, err := ioutil.ReadFile(file)
 
+		file = filepath.ToSlash(file)
+
 		if strings.Contains(file, ".ssc") {
 			continue
 		}
 
 		bloblength := len(string(raw))
-		content := "blob " + string(rune(bloblength)) + string(raw) // Hash = string "blob" + space + len of blob + raw content
+
+		/*Blob hash is calculated like:
+		blob $LENGTHOFBLOB $BLOB
+		*/
+		content := "blob " + string(rune(bloblength)) + string(raw)
 
 		if err != nil {
 			panic(err)
@@ -138,11 +172,14 @@ func createBlobs(filenames []string) string {
 		hash := hex.EncodeToString(hasher.Sum(nil))
 		hashfile := ".ssc/objects/" + hash
 
+		// If the file doesn't exist, create it
+		// Multiple files can point to the same object
 		if !fileExists(hashfile) {
 			ioutil.WriteFile(hashfile, raw, 0644)
 			zlibfiles[hashfile] = hash
 		}
 
+		// If last element in array, don't append string with newline
 		if index != len(filenames)-1 {
 			treestring += file + " " + hash + "\n"
 		} else {
