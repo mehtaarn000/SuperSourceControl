91012bc4fbfd1bac5479e769cc2cdf49626a902b
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2026-08-03 04:10:50 -0400

    Add more descriptive functions to core directory && remove redundency

--- core/init.go
+++ core/init.go
@@ -8,23 +8,37 @@ package core
 import "os"
 
 func Init() {
+	// If .ssc repo already exists
 	if _, err := os.Stat(".ssc"); err != nil {
 		if os.IsExist(err) {
 			print("Repository already exists.")
 			os.Exit(0)
 		}
 	}
-	os.MkdirAll(".ssc/branches", 0777)
-	os.MkdirAll(".ssc/objects", 0777)
-	os.MkdirAll(".ssc/tmp", 0777)
 
+	/*Repository file structure:
+	.ssc/
+	|-- branches/
+	|-- objects/
+	|-- tmp/
+	|-- branch (default = master)
+	|-- commitlog
+	|-- trees	
+	*/
+
+	// Create dirs
+	err := os.MkdirAll(".ssc/branches", 0777)
+	err = os.MkdirAll(".ssc/objects", 0777)
+	err = os.MkdirAll(".ssc/tmp", 0777)
+
+	// Create files
 	f, err := os.Create(".ssc/branch")
 	f.WriteString("master")
+
+	f, err = os.Create(".ssc/commitlog")
+	f, err = os.Create(".ssc/trees")
 
 	if err != nil {
 		panic(err)
 	}
-
-	os.Create(".ssc/commitlog")
-	os.Create(".ssc/trees")
 }
--- core/log.go
+++ core/log.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 )
 
+// Utility function to see if any item in the array is a newline, remove it
 func delete_empty(s []string) []string {
 	var r []string
 	for _, str := range s {
@@ -20,6 +21,7 @@ func delete_empty(s []string) []string {
 	return r
 }
 
+// Exactly what is sounds like
 func reverseArray(input []string) []string {
 	if len(input) == 0 {
 		return input
@@ -29,6 +31,7 @@ func reverseArray(input []string) []string {
 
 // Log lists the commits from the commitlog
 func Log(commits int, reverse bool) {
+	// Get commits
 	bytescommitlog, err := ioutil.ReadFile(".ssc/commitlog")
 	commitlog := strings.Split(string(bytescommitlog), "\n")
 	commitlog = commitlog[:len(commitlog)-1]
@@ -37,21 +40,29 @@ func Log(commits int, reverse bool) {
 		panic("Number of requested commits is too large")
 	}
 
+	// Get number of commits passed to function
 	requested_commits := commitlog[len(delete_empty(commitlog))-commits:]
 	if err != nil {
 		panic(err)
 	}
 
+	// If the user doesn't specify the reverse option
 	if !reverse {
 		requested_commits = reverseArray(requested_commits)
 	}
 
 	for _, commit := range requested_commits {
+		// Get content of each commit
 		content := getContent(commit)
 		split_content := strings.Split(content, "\n")
 
+		// Slice the string to get the date and message
 		date := split_content[1][5:]
 		message := split_content[4]
+
+		/*Output looks like:
+		$COMMITHASH   $COMMITDATEANDTIME   $COMMITMESSAGE
+		*/
 		print(commit, "   ", date, "   ", message, "\n")
 	}
 
@@ -60,6 +71,7 @@ func Log(commits int, reverse bool) {
 	}
 }
 
+// MaxLog gets all the commits and logs them
 func MaxLog(reverse bool) {
 	bytescommitlog, err := ioutil.ReadFile(".ssc/commitlog")
 	numofcommits := len(strings.Split(string(bytescommitlog), "\n"))
